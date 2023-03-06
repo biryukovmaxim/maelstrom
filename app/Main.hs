c6d6fcaec67e6df939881d1b96ea6ea530c05b82
maksim <maksim@blockchaincmdt.io>
2023-03-06 16:24:27 +0400

    init handshake works

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,12 @@
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE Strict #-}
 
-import qualified Data.ByteString.Lazy as BS
-import Node (emptyNode, handleRawMessage)
-import Data.Maybe (fromJust)
-import qualified Data.ByteString.Lazy.Char8 as BC
+import Node (emptyNode, handleRawMessage, Node)
+import qualified Data.ByteString.Lazy as BSL
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Builder as BSB
+
+import System.IO (hSetBuffering, stdin, BufferMode(..), hFlush, stdout)
 
 -- main :: IO ()
 -- main = loop emptyNode
@@ -17,9 +20,23 @@ import qualified Data.ByteString.Lazy.Char8 as BC
 --        Nothing -> loop node
 
 main :: IO ()
+
 main = do
-  input <- BS.getContents
-  let result = $! handleRawMessage emptyNode  input
-    in putStrLn $! BC.unpack $ snd (fromJust result)
-  
-  
+  hSetBuffering stdin LineBuffering
+  loop emptyNode
+
+loop :: Node -> IO ()
+loop node = do
+  input <- BS.hGetLine stdin
+  let lazyInput = BSL.fromStrict input
+  let nodeAndMsg = handleRawMessage node lazyInput
+  case nodeAndMsg of
+    Just (newNode, outPutMsg) -> do
+      BSL.putStr $ appendNewline outPutMsg
+      hFlush stdout
+      loop newNode
+    Nothing -> loop node
+      
+
+appendNewline :: BSL.ByteString -> BSL.ByteString
+appendNewline bs = BSB.toLazyByteString $ BSB.lazyByteString bs <> BSB.charUtf8 '\n'
