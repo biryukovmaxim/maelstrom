50c8b0816a10af24cf75c29e6d1f1518d4e30b99
max143672 <biryukov.maxim.vladimirovich@yandex.ru>
2023-03-06 22:31:40 +0400

    all-good

--- src/Message.hs
+++ src/Message.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE Strict #-}
 
 module Message
@@ -10,11 +9,11 @@ module Message
 where
 
 import Data.Aeson
+import Data.Aeson.Key (fromString)
+import qualified Data.HashMap.Strict as HM
+import Data.Maybe (catMaybes)
 import qualified Data.Text as T
 import GHC.Generics
-import EncodeOptions(msgBodyOptions)
-import Data.Aeson.TH (deriveJSON)
-import Data.HashMap.Strict (HashMap)
 
 data Message = Message
   { src :: String,
@@ -31,7 +30,7 @@ data MessageBody = MessageBody
     text :: Maybe String,
     nodeId :: Maybe String,
     nodeIds :: Maybe [String],
-    other :: Maybe (HashMap String Value) -- catch-all field
+    other :: HM.HashMap String Value -- catch-all field
   }
   deriving (Show, Eq, Generic)
 
@@ -50,5 +49,43 @@ instance ToJSON MessageType where
   toJSON InitOkType = String $ T.pack "init_ok"
   toJSON (CustomMessageType s) = String (T.pack s)
 
-$(deriveJSON msgBodyOptions ''MessageBody)
-$(deriveJSON defaultOptions ''Message)
+instance FromJSON MessageBody where
+  parseJSON = withObject "MessageBody" $ \o -> do
+    let knownFields = ["type", "msg_id", "in_reply_to", "code", "text", "node_id", "node_ids"]
+    unknownFields <- flip HM.filterWithKey <$> parseJSON (Object o) <*> pure (\key -> \_ -> key `notElem` knownFields)
+    msgType <- o .: fromString "type"
+    msgId <- o .:? fromString "msg_id"
+    inReplyTo <- o .:? fromString "in_reply_to"
+    code <- o .:? fromString "code"
+    text <- o .:? fromString "text"
+    nodeId <- o .:? fromString "node_id"
+    nodeIds <- o .:? fromString "node_ids"
+    return $ MessageBody msgType msgId inReplyTo code text nodeId nodeIds unknownFields
+
+instance ToJSON MessageBody where
+  toJSON (MessageBody msgType msgId inReplyTo code text nodeId nodeIds other) =
+    let knownFields =
+          [ Just $ fromString "type" .= msgType,
+            fromString "msg_id" .=? msgId,
+            fromString "in_reply_to" .=? inReplyTo,
+            fromString "code" .=? code,
+            fromString "text" .=? text,
+            fromString "node_id" .=? nodeId,
+            fromString "node_ids" .=? nodeIds
+          ]
+        unknownFields = HM.toList other
+        allFields = catMaybes knownFields ++ map (\(k, v) -> fromString k .= v) unknownFields
+     in object allFields
+
+instance FromJSON Message
+
+instance ToJSON Message
+
+-- Helper function to encode optional fields
+(.=?) ::
+  ToJSON a =>
+  Key ->
+  Maybe a ->
+  Maybe (Key, Value)
+(.=?) _ Nothing = Nothing
+(.=?) k (Just v) = Just (k .= v)
