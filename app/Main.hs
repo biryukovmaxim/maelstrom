39fa358855f9549790c46824d3b87968146e3346
maksim <maksim@blockchaincmdt.io>
2023-03-07 11:26:33 +0400

    echo - demo

--- app/Main.hs
+++ app/Main.hs
@@ -13,10 +13,6 @@ import System.IO
     stdin,
     stdout,
   )
-import Data.UUID.V4 (nextRandom)
-import Data.UUID(toText)
-import Data.HashMap.Strict (insert)
-import qualified Data.Aeson.Types as T
 
 main :: IO ()
 main = do
@@ -49,11 +45,4 @@ myNode =
 
 myCustomHandler :: String -> MessageBody -> IO (Maybe MessageBody)
 myCustomHandler "echo" inputBody = pure $ Just inputBody {inReplyTo = msgId inputBody, msgType = CustomMessageType "echo_ok"}
-myCustomHandler "generate" inputBody = do
-   randomUUID <- nextRandom
-   let uuidString =  T.String $ toText randomUUID
-   let oldOther = other inputBody
-   let newOther = insert "id" uuidString oldOther
-   pure $ Just inputBody {inReplyTo = msgId inputBody, msgType = CustomMessageType "generate_ok", other = newOther}
-
 myCustomHandler _ _ = pure Nothing
